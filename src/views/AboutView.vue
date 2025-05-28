667ced76b49c7aba455a2487202d68ae498f7a01
Sett Sarverott <sarverott@gmail.com>
2025-05-28 15:27:38 +0200

    Auto-commit - Wed May 28 03:27:38 PM CEST 2025

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,11 +3,12 @@
     <iframe
       width="425"
       height="350"
-      src="https://www.openstreetmap.org/export/embed.html?bbox=19.006626605987552%2C50.24783609436576%2C19.034950733184818%2C50.26202200877068&amp;layer=transportmap"
+      src="https://www.openstreetmap.org/export/embed.html?bbox=19.0141900%2C50.2537500%2C19.0230850%2C50.2579890&amp;layer=transportmap&amp;marker=50.2548944234351%2C19.020088613033295"
       style="border: 1px solid black"
     ></iframe
     ><br /><small
-      ><a href="https://www.openstreetmap.org/?#map=16/50.25493/19.02079&amp;layers=TN"
+      ><a
+        href="https://www.openstreetmap.org/?mlat=50.2548944&amp;mlon=19.0200886#map=21/50.2548944/19.0200886&amp;layers=TN"
         >View Larger Map</a
       ></small
     >
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,19 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <TheWelcome />
+    <!-- Replace the variables below. -->
+    <script>
+      function onSubmit(token) {
+        document.getElementById('demo-form').submit()
+      }
+    </script>
+    <button
+      class="g-recaptcha"
+      data-sitekey="6LejN0wrAAAAAHYS1ampdaZpdG5GBvCgQB-u3j8a"
+      data-callback="onSubmit"
+      data-action="submit"
+    >
+      Submit
+    </button>
   </main>
 </template>
